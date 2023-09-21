a86a1eb3165e47a3b380c23bb4fc0a3223b467bc
natsukashiixo <114266981+natsukashiixo@users.noreply.github.com>
2023-09-21 13:00:05 +0200

    prefer natural sorting to creation time sort

--- app/src/modules/doublepage_img_rename.py
+++ app/src/modules/doublepage_img_rename.py
@@ -3,34 +3,44 @@ from modules.is_image import is_image as IsImage
 import os
 import shutil
 from modules.functions_ui import ProgressCounter
+import re
+from natsort import os_sorted
 
 rootfolder = './ImportFolder' 
 destination = Path('./SplitterInput/')
 
 def double_rename(rootfolder=rootfolder, destination=destination):
-    print('Renaming and moving files based on creation date')
     allfiles = list(Path(rootfolder).rglob('*.*'))
     ImageList = []
     RenamedFiles = []
     Counter = '0001'
-
-    try:
-        for file in allfiles:
-            if IsImage(file): 
-                os.path.getctime(file)
-                #print(file, 'was created at:', os.path.getctime(file), 'in UNIX time') # Read file creation time for each image file using os.path.getctime
-                ImageList.append(file)
-            else:
-                print(file, "is not an image")
-    except BaseException as error:
-            print('An exception occurred while processing {}: {}'.format(file, error))
+    has_numbers = re.compile(r'[0-9]')
+    
+    if all(has_numbers.search(str(file)) for file in allfiles):
+        print('All images are numbered, using Windows sorting')
+        ImageList = [file for file in allfiles if IsImage(file)]
+        ImageList = os_sorted(ImageList)
+    
+    else:
+        print("Images aren't ordered, ordering based on file creation date")
+        try:
+            for file in allfiles:
+                if IsImage(file): 
+                    os.path.getctime(file)
+                    #print(file, 'was created at:', os.path.getctime(file), 'in UNIX time') # Read file creation time for each image file using os.path.getctime
+                    ImageList.append(file)
+                else:
+                    print(file, "is not an image")
+        except BaseException as error:
+                print('An exception occurred while processing {}: {}'.format(file, error))
+        ImageList = sorted(ImageList, key=os.path.getctime)
 
     # Rename each file sequentially using the order found in ImageList, incrementing Counter by 1 with each file read
 
     files = list(filter(IsImage, allfiles))
 
     with ProgressCounter(len(files)) as progress:
-        for file in sorted(ImageList, key=os.path.getctime):
+        for file in ImageList:
             try:
             #print(file, os.path.getctime(file)) #Debug statement
                 ext = os.path.splitext(file)[1] # Save the file extension
