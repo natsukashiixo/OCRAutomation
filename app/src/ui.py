274a9408843fe8975ec53f0ac3a6d5dcbd876cb9
natsukashiixo <114266981+natsukashiixo@users.noreply.github.com>
2023-10-03 14:07:06 +0200

    this changes nothing

--- app/src/ui.py
+++ app/src/ui.py
@@ -1,7 +1,5 @@
 #I'm sorry if someone has to handle this after me haha this is a mess.
 
-#lets just write something to test the new workflow
-
 # -*- coding: utf-8 -*-
 
 # Form implementation generated from reading ui file '.\advanced.ui'
