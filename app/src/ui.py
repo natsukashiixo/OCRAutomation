5e2b8a19d25f923b8e074307f94252a334c12a55
natsukashiixo <114266981+natsukashiixo@users.noreply.github.com>
2023-10-03 14:10:37 +0200

    git is very complicated for me

--- app/src/ui.py
+++ app/src/ui.py
@@ -1,5 +1,7 @@
 #I'm sorry if someone has to handle this after me haha this is a mess.
 
+#i dont know how workflows work
+
 # -*- coding: utf-8 -*-
 
 # Form implementation generated from reading ui file '.\advanced.ui'
